06be0e357cac673a9e87745281e139ddf6901011
ilof2 <ilof@tuta.io>
2025-10-03 16:55:27 +0200

    More lsp twicks

--- .config/nvim/lua/user/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/user/plugins/lsp/lspconfig.lua
@@ -22,24 +22,18 @@ return {
 					},
 					capabilities = capabilities,
 					filetypes = filetypes,
-					-- on_attach = function(client, bufnr)
-					-- 	require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
-					-- end,
 				})
 				return
 			end
 			vim.lsp.config(server_name, {
 				capabilities = capabilities,
 				filetypes = filetypes,
-				-- on_attach = function(client, bufnr)
-				-- 	require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
-				-- end,
 			})
 		end
 
 		local servers = {
 			pyright = { "python" },
-			html = { "html" },
+			htmllsp = { "html" },
 			cssls = { "css" },
 			eslint = { "js" },
 			lua_ls = { "lua" },
@@ -52,7 +46,6 @@ return {
 		}
 		for lsp, filetypes in pairs(servers) do
 			setup_service(lsp, filetypes)
-      vim.lsp.enable(lsp)
 		end
 	end,
 }
